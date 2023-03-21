1292d882648235ba451f5675a22a1e4df6399d58
plla2 <jhbljs92@naver.com>
2023-03-21 14:11:21 +0900

    데일리 코딩 문제풀이

--- main.js
+++ main.js
@@ -1,32 +1,10 @@
-// let arr = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
-// let output = transformFirstAndLast(arr);
-// console.log(output); // --> { Queen : 'Beyonce' }
+let output1 = letterCapitalize('hello world');
+console.log(output1); // "Hello World"
+let output2 = letterCapitalize('javascript  is sexy ');
+console.log(output2); // "Javascript  Is Sexy "
 
-// arr = ['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey'];
-// output = transformFirstAndLast(arr);
-// console.log(output); // --> { Kevin : 'Spacey' }
-
-// function transformFirstAndLast(arr) {
-//   let sol = {};
-//   if (arr.length === 0) { return {} }
-//   sol[arr[0]] = arr[arr.length - 1];
-//   return sol;
-// }
-
-
-// // 함수 객체는 prototype 프로퍼티를 소유한다.
-// (function () {}).hasOwnProperty('prototype');  // true
-
-// // 일반 객체는 prototype 프로퍼티를 소유하지않는다.
-// ({}).hasOwnProperty('prototype');  // false
-
-// 화살표 함수는 non-constructor
-const Person = name => {
-  this.name = name;
+function letterCapitalize(str) {
+  // TODO: 여기에 코드를 작성합니다.
+  let newStr = str.replace(/\b[a-z]/g, char => char.toUpperCase());
+  return (newStr);
 }
-
-// non-constructor는 prototype 프로퍼티를 소유하지 않음
-console.log(Person.hasOwnProperty('prototype'));  // false
-
-// non-constructor는 프로토타입을 생성하지 않음
-console.log(Person.prototype);  // undefined
