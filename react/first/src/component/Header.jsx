eb1a9b187f0e4a265b58aa3ea8043d1894d7f80a
plla2 <jhbljs92@naver.com>
2023-03-23 09:46:39 +0900

    컴포넌트 블로깅 코드자료

---
+++ react/first/src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1>이곳은 헤더~~~~~</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
---
+++ react/블로깅자료/컴포넌트.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function MyComponent() {
+  return <h1>Hello!! 승현!</h1>
+}
+export default MyComponent;
+
+import React from 'react'
+class MyComponent extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+
+  }
+  render() {
+    return <h1>Hello!! 승현!</h1>
+  }
+}
+// export default MyComponent
