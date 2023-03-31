9b10f2919c7080015d438b4153c7dcc7a4e75f8a
plla2 <jhbljs92@naver.com>
2023-03-31 13:19:06 +0900

    코플릿, 프로그래머스 풀이

--- main.js
+++ main.js
@@ -77,20 +77,92 @@
 // console.log(message1); // --> 30
 // let message2 = solution("l love you~");
 // console.log(message2); // --> 22
-let rsp = solution("205");
+// let rsp = solution("205");
 
-function solution(rsp) {
-  let result = "";
-  rsp = rsp.split("")
-  console.log(rsp);
-  for (let i = 0; i < rsp.length; i++) {
-    if (rsp[i] === 0) {
-      return result += "5"
-    } else if (rsp[i] === 2) {
-      return result += "0"
-    } else {
-      return result += "2"
+// function solution(rsp) {
+//   let result = "";
+//   rsp = rsp.split("")
+//   console.log(rsp);
+//   for (let i = 0; i < rsp.length; i++) {
+//     if (rsp[i] === 0) {
+//       return result += "5"
+//     } else if (rsp[i] === 2) {
+//       return result += "0"
+//     } else {
+//       return result += "2"
+//     }
+//   }
+//   return result;
+// }
+
+
+// function insertDash(str) {
+//   let result = '';
+//   for (let i = 0; i < str.length; i++) {
+//     if (i > 0 && (str[i] % 2 === 1) && (str[i - 1] % 2 === 1)) {
+//       result += '-';
+//     }
+//     result += str[i];
+//   }
+//   return result;
+// }
+
+// let output = insertDash('454793')
+// console.log(output)
+
+
+// function removeExtremes(arr) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let result = [];
+
+//   longestLen = 0;
+//   longestIdx = 0;
+//   shortestLen = 20;
+//   shortestIdx = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length >= longestLen) {
+//       longestLen = arr[i].length;
+//       longestIdx = i;
+//     }
+//     if (arr[i].length <= shortestLen) {
+//       shortestLen = arr[i].length;
+//       shortestIdx = i;
+//     }
+//   }
+//   for (let j = 0; j < arr.length; j++) {
+//     if (j !== longestIdx && j !== shortestIdx) {
+//       result.push(arr[j]);
+//     }
+//   }
+//   return result;
+// }
+
+
+// let output = removeExtremes(['a', 'b', 'c', 'def']);
+// console.log(output); // --> ['a', 'b']
+
+// output = removeExtremes(['where', 'is', 'the', 'longest', 'word']);
+// console.log(output); // --> ['where', 'the', 'word',]
+
+
+function findBugInApples(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes('B')) {
+      let index = arr[i].indexOf('B');
+      return [i, index];
     }
   }
-  return result;
 }
+
+let output = findBugInApples([['A'], ['B']]);
+console.log(output); //[1, 0]
+
+output = findBugInApples([
+  ['A', 'A', 'A', 'A', 'A'],
+  ['A', 'B', 'A', 'A', 'A'],
+  ['A', 'A', 'A', 'A', 'A'],
+  ['A', 'A', 'A', 'A', 'A'],
+  ['A', 'A', 'A', 'A', 'A'],
+]);
+console.log(output); //[1, 1]
