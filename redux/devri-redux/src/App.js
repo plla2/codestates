e9b30f8a4f35644d717d8cc4aeff990bf1e56607
plla2 <jhbljs92@naver.com>
2023-04-26 10:49:16 +0900

    리덕스-리액트활용 간단페이지구현

---
+++ redux/devri-redux/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Display from './components/Display';
+import Subscribers from './components/Subscribers';
+
+function App() {
+  return (
+    <div className="App">
+      <Subscribers />
+      <Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ redux/devri-redux/src/components/Subscribers.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { addSubscriber } from '../redux/subscribers/actions';
+
+
+function Subscribers({ count, addSubscriber }) {
+  return (
+    <div className='items'>
+      <h2>구독자 수: {count}</h2>
+      <button onClick={() => addSubscriber()}>구독하기</button>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.count
+  }
+}
+const mapDispatchToProps = {
+  addSubscriber
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Subscribers)
