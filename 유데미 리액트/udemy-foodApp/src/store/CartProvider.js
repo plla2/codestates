9fc01c85a7332d1d55d30e56ef9e25631ed4c2e0
plla2 <jhbljs92@naver.com>
2023-05-20 10:57:15 +0900

    장바구니 리듀서 추가

--- 유데미 리액트/udemy-foodApp/src/store/CartProvider.js
+++ 유데미 리액트/udemy-foodApp/src/store/CartProvider.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import CartContext from './cart-context'
 
+const initialState = {
+  items: [],
+  totalAmount: 0
+}
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+  return initialState;
+}
+
 function CartProvider(props) {
-  const addItemToCartHandler = item => { };
-  const removeItemFromCartHandler = id => { }
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, initialState)
+  const addItemToCartHandler = item => {
+    dispatchCartAction({ type: "ADD", item: item })
+  };
+  const removeItemFromCartHandler = id => {
+    dispatchCartAction({ type: "REMOVE", id: id })
+  }
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   }
