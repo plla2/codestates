6d948f3ec4bd56c8ef9a3bfe69761234b36d3ee1
plla2 <jhbljs92@naver.com>
2023-05-21 11:23:44 +0900

    Ref, Forward Ref 작업하기

--- 유데미 리액트/udemy-foodApp/src/components/Meals/MealItem/MealItemForm.js
+++ 유데미 리액트/udemy-foodApp/src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import classes from "./MealItemForm.module.css"
 import Input from '../../UI/Input'
 
 function MealItemForm(props) {
+  const [amountIsValid, setAmountIsValid] = useState(true)
+  const amountInputRef = useRef()
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredAmount = amountInputRef.current.value
+    const enteredAmountNumber = +enteredAmount
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+      setAmountIsValid(false)
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  }
+
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={{
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input ref={amountInputRef} label="Amount" input={{
         id: "amount_" + props.id,
         type: "number",
         min: "1",
@@ -14,6 +29,7 @@ function MealItemForm(props) {
         defaultValue: '1',
       }} />
       <button>+ Add</button>
+      {!amountIsValid && <p>유효한 수량 (1-5)를 입력하세요.</p>}
     </form>
   )
 }
