9fae01eb55355abf1de52e105098dbd4b9cbdb62
plla2 <jhbljs92@naver.com>
2023-05-22 19:01:35 +0900

    섹션11 연습 프로젝트 애니메이션 기능까지 완

--- 유데미 리액트/udemy-foodApp/src/components/Layout/HeaderCartButton.js
+++ 유데미 리액트/udemy-foodApp/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import classes from "./HeaderCartButton.module.css"
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context'
 
 function HeaderCartButton(props) {
+  const [btnHighlight, setBtnHighlight] = useState(false);
   const cartCtx = useContext(CartContext)
-
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${btnHighlight ? classes.bump : ""}`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighlight(true);
+    const timer = setTimeout(() => {
+      setBtnHighlight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
   return (
-    <button className={classes.button} onClick={props.onClick} >
+    <button className={btnClasses} onClick={props.onClick} >
       <span className={classes.icon}>
         <CartIcon />
       </span>
