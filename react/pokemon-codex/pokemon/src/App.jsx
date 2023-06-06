234bfaaa234413fa45f805e033e7586c59d00ba6
plla2 <jhbljs92@naver.com>
2023-06-06 19:38:24 +0900

    fetch pokemons infos

--- react/pokemon-codex/pokemon/src/App.jsx
+++ react/pokemon-codex/pokemon/src/App.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+    setLoadMore(data.next);
+
+    const createPokemonObject = (result) => {
+      result.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
+        setAllPokemons((currentList) => [...currentList, data]);
+      });
+    };
+    createPokemonObject(data.results);
+    await console.log(allPokemons);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
   return (
     <div className="app-container">
       <h1>Pokemon Evolution</h1>
+      <div className="pokemon-container">
+        <div className="all-container"></div>
+        <button className="load-more">Load more</button>
+      </div>
     </div>
   );
 }
